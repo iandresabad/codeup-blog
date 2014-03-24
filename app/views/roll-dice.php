94be1643d61380d6f50a77cbf8542ae27b5262e5
iandresabad <iandresabad@yahoo.com>
2014-03-24 14:37:08 -0500

    random Dice

---
+++ app/views/roll-dice.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<title>Roll Dice</title>
+</head>
+<body>
+	<h1>Dice roll was: <?php echo $random;?></h1>
+	<h1>Your guess was: <?php echo $guess; ?></h1>
+	<h1><?php echo $message; ?></h1>
+</body>
+</html>
