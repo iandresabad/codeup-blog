e854f2f07c7d7a171f4b7042ea7a9a1712e31abe
iandresabad <iandresabad@yahoo.com>
2014-03-27 11:15:39 -0500

    orm-test

--- app/routes.php
+++ app/routes.php
@@ -11,15 +11,28 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+
+Route::get('/', 'HomeController@showWelcome');
+
+Route::get('orm-test', function () {
+	$posts = Post::all();
+	return $posts;
+	// // // // Post::find(2); option 2
+	foreach ($posts as $post) {
+	// // // 	//take out the foreach if using option 2 
+	echo '<h2>' . $post->title . '</h2>' . '<p>' . $post->body . '</p>';
+	}
+	// $post1 = new Post();
+	// $post1->title = "Eloquent is awesome!";
+	// $post1->body = "It is super easy to create a new post.";
+	// $post1->save();
+
+	// $post2 = new Post();
+	// $post2->title = "Post number two";
+	// $post2->body = "The body for post number two.";
+	// $post2->save();
 });
 
-Route::get('/sayhello/{name}', function($name)
-{
-    return View::make('my-first-view')->with('name', $name);
-});
 
 Route::get('/resume', 'HomeController@showResume');
 
@@ -27,16 +40,3 @@ Route::get('/resume', 'HomeController@showResume');
 Route::get('/portfolio', 'HomeController@showPortfolio');
 
 Route::resource('posts', 'PostsController');
-
-Route::get('/rolldice/{guess?}', function($guess = 0)
-{
-	$rand = mt_rand(1, 6);
-	if ($rand == $guess) {
-		$result = 'You guessed correctly!';
-	} else {
-		$result = "You guessed wrong!";
-	}
-
-	$data = array("guess" => $guess, "rand" => $rand, 'result' => $result);
-	return View::make('roll-dice')->with($data);
-});
